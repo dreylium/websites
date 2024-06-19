1fffbb562aa06359d3a2e601017f8bfd8c72cc00
dreylium <dreylium@gmail.com>
2024-06-19 19:37:25 +0700

    update

--- dreylium/src/lib/data.js
+++ dreylium/src/lib/data.js
@@ -3,23 +3,19 @@ import tailwindcss from '$lib/images/min/logo/tailwindcss.svg';
 import eslint from '$lib/images/min/logo/eslint.svg';
 import prettier from '$lib/images/min/logo/prettier.svg';
 
+
+
 export const webs = [
 	{
 		name: 'shortly',
-		visit: 'https://dreylium.netlify.app/show/shortly/',
-		code: 'https://github.com/dreylium/portfolio/tree/code/shortly',
+		visit: 'https://dreylium-shortly.vercel.app',
+		code: 'https://github.com/dreylium/websites/tree/main/shortly',
 		source: 'https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G'
 	},
 	{
-		name: 'insure',
-		visit: 'https://dreylium.netlify.app/show/insure/',
-		code: 'https://github.com/dreylium/portfolio/tree/code/insure',
-		source: 'https://www.frontendmentor.io/challenges/insure-landing-page-uTU68JV8'
-	},
-	{
 		name: 'dreylium',
-		visit: 'https://dreylium.netlify.app',
-		code: 'https://github.com/dreylium/portfolio/tree/code/portfolio',
+		visit: 'https://dreylium.vercel.app',
+		code: 'https://github.com/dreylium/websites/tree/main/dreylium',
 		source: 'https://github.com/dreylium'
 	}
 ];
